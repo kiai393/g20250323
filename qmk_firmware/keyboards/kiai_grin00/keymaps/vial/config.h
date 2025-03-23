8783e2b10b4a813a2686890fd39abc264ef9c04b
kiai393 <jchikaj@yahoo.co.jp>
2025-03-23 17:24:37 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/kiai_grin00/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+#define VIAL_KEYBOARD_UID {0xe4, 0x95, 0xea, 0xc6, 0x9f, 0x3e, 0xb5, 0x4a}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
